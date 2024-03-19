3b5ffdd1c97bbe409b8dc6176cd715329823cbdd
Nicollas Bonfim Severo da Silva <154918468+NicollasSevero@users.noreply.github.com>
2024-03-18 22:26:27 -0300

    update exercicies py

--- desafio014.py
+++ desafio014.py
@@ -1,1 +1,6 @@
 #Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+
+temp = float(input("Informe a temperatura em ºC: "))
+fire = ((9*temp)/5) + 32
+        
+print('A temperatura de {}ºC corresponde a {} F!'.format(temp, fire))
